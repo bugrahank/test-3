64b5d6d595227641d3ec7c55a97c8eca62685d86
Bugrahan Kadak <kadak.bugrahan@std.izu.edu.tr>
2021-01-09 09:29:21 +0300

    init commit

---
+++ bootstrapt.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css"
+
+    <title>Hello, world!</title>
+    <hr>
+</head>
+<body>
+<h1>Hello, world!</h1>
+<div class="container">
+    <div class="row">
+
+        <table class="p-5 mt-5 table border border-dark">
+            <tr>
+                <td>1.Kolon</td>
+                <td>2.Kolon</td>
+                <td>3.Kolon</td>
+            </tr>
+            <tr>
+                <td>1.Kolon</td>
+                <td>2.Kolon</td>
+                <td>3.Kolon</td>
+            </tr>
+            <tr>
+                <td>1.Kolon</td>
+                <td>2.Kolon</td>
+                <td>3.Kolon</td>
+            </tr>
+        </table>
+    </div>
+
+
+
+
+
+
+
+            <hr>
+
+        <table>
+            <tr>
+                <td><button type="button" class="btn btn-primary btn-sm">Primary </button></td>
+                <td><button type="button" class="btn btn-outline-primary">Primary </button></td>
+            </tr>
+            <tr >
+                <td><button type="button" class="btn btn-secondary">Secondary </button></td>
+                <td><button type="button" class="btn btn-outline-secondary">Primary </button></td>
+
+            </tr>
+            <tr >
+                <td><button type="button" class="btn btn-info">Info </button></td>
+                <td><button type="button" class="btn btn-outline-info ">Info </button></td>
+
+            </tr>
+            <tr >
+                <td><button type="button" class="btn btn-danger">Danger</button></td>
+                <td><button type="button" class="btn btn-outline-danger">Danger </button></td>
+
+            </tr>
+            <tr>
+                <td><button type="button" class="btn btn-warning">Warning</button></td>
+            </tr>
+            <tr >
+                <td><button type="button" class="btn btn-success btn-block">Success</button></td>
+            </tr>
+            <tr >
+                <td><button type="button" class="btn btn-dark">Dark</button></td>
+            </tr>
+
+        </table>
+
+
+
+        <div class="w-100"></div>
+
+        <table>
+            <tr>
+                <td> <span class="text-primary>">Renk Kodu </span> </td>
+            </tr>
+            <tr >
+                <td><span class ="text-info">Renk Kodu </span>></td>
+            </tr>
+            <tr >
+                <td><span class ="text-danger">Renk Kodu </span>></td>
+            </tr>
+            <tr >
+                <td><span class ="text-warning">Renk Kodu </span>></td>
+            </tr>
+            <tr>
+                <td><span class ="text-success">Renk Kodu </span>></td>
+            </tr>
+            <tr >
+                <td><span class ="text-dark">Renk Kodu </span>></td>
+            </tr>
+            <tr >
+                <td><span class ="text-light">Renk Kodu </span>></td>
+            </tr>
+
+        </table>
+
+        <hr>
+
+        <table>
+            <tr class="bg-primary">
+                <td>Renk Kodu </td>
+            </tr>
+            <tr class="bg-info">
+                <td>Renk Kodu </td>
+            </tr>
+            <tr class="bg-danger">
+                <td>Renk Kodu </td>
+            </tr>
+            <tr class="bg-warning">
+                <td>Renk Kodu </td>
+            </tr>
+            <tr class="bg-success">
+                <td>Renk Kodu </td>
+            </tr>
+            <tr class="bg-dark">
+                <td>Renk Kodu </td>
+            </tr>
+            <tr class="bg-light">
+                <td>Renk Kodu </td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+
+
+
+
+<script src="js/jquery-3.5.1.slim.min.js"> </script>
+<script src="js/bootstrap.bundle.min.js" ></script>
+
+
+</body>
+</html>
